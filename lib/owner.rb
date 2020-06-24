af0b3ad2717e066ab228d2a630c05af1a9c406b0
Jack Goode <jack.w.goode@icloud.com>
2020-06-24 21:23:07 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
 class Owner
-  # code goes here
+    
+   attr_reader :species, :name
+   
+   @@all = []
+   
+   def initialize(name)
+     @species = "human"
+     @name = name
+     @@all << self
+   end
+   
+   def say_species
+     "I am a #{self.species}."
+   end
+   
+   def self.all 
+       @@all
+   end
+   
+   def self.count
+      self.all.count
+   end
+   
+   def self.reset_all
+     self.all.clear
+   end
+   
+   def cats
+     Cats.all.select do |cat|
+     cat.owner == self.name
+    end
+  end
+  
+  def buy_cat(name)
+    cat = Cat.new(name, self)
+  end
+  
+  def buy_dog 
+    
+  end
+   
 end
