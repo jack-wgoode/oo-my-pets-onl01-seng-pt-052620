af816fdac7424150f85ac3b36ad71a1b508c12ab
Jack Goode <jack.w.goode@icloud.com>
2020-06-24 23:18:13 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -71,7 +71,7 @@ class Owner
    end
    
    def list_pets
-    print  "I have #{@owner.dogs.count}dog(s), and #{@owner.cats.count} cat(s)."   
+    print  "I have #{dogs.count}dog(s), and #{cats.count} cat(s)."   
   end
    
    
